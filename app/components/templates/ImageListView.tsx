5dccecb661bb4bb6a4fdf8b5833eb8d13f65b03a
msrks <mshrrks@gmail.com>
2023-04-08 02:17:22 +0900

    fix: limit query of numImages for fast-page-load

--- app/components/templates/ImageListView.tsx
+++ app/components/templates/ImageListView.tsx
@@ -14,8 +14,19 @@ import {
   Theme,
   useMediaQuery,
 } from "@mui/material";
-import { ChangeEvent, FC, useState } from "react";
-import { collection, deleteDoc, DocumentReference, orderBy, Query, query, QuerySnapshot, updateDoc, where } from "firebase/firestore";
+import { ChangeEvent, Dispatch, FC, SetStateAction, useState } from "react";
+import {
+  collection,
+  deleteDoc,
+  DocumentReference,
+  limit,
+  orderBy,
+  Query,
+  query,
+  QuerySnapshot,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { Image, lightingCondition } from "../../types/image";
 import { useCollection } from "react-firebase-hooks/firestore";
 import Title from "../../components/elements/Title";
@@ -26,6 +37,7 @@ import { AdapterDateFns } from "@mui/x-date-pickers-pro/AdapterDateFns";
 import { DateRange, DateRangePicker, LocalizationProvider } from "@mui/x-date-pickers-pro";
 import { endOfDay } from "date-fns";
 import { Masonry } from "@mui/lab";
+// import { InView, useInView } from "react-intersection-observer";
 
 const colorDict = (bool: boolean) => (bool ? cyan[600] : red[600]);
 
@@ -34,19 +46,25 @@ interface Props {
 }
 
 const ImageListView: FC<Props> = ({ lightingCondition }) => {
+  const [page, setPage] = useState(1);
   const q = query(
     collection(db, "images"),
     where("lightingCondition", "==", lightingCondition),
-    orderBy("createdAt", "desc")
+    orderBy("createdAt", "desc"),
+    limit(100)
   ) as Query<Image>;
   const [snap, loading, error] = useCollection<Image>(q);
-
   return (
     <>
       {error && <Typography m="auto">⛔️ {error.message}</Typography>}
       {loading && <CircularProgress sx={{ m: "auto" }} />}
       {snap && snap.docs.length == 0 && <Typography m="auto">⛔️ There is no images</Typography>}
-      {snap && snap.docs.length > 0 && <ImageListViewMain title={capitalize(lightingCondition)} snap={snap} />}
+
+      {snap && snap.docs.length > 0 && (
+        // <InfiniteScroll loadMore={() => setPage((p) => p + 1)} hasMore={true} loader={<>loading</>}>
+        <ImageListViewMain title={capitalize(lightingCondition)} snap={snap} setPage={setPage} />
+        // </InfiniteScroll>
+      )}
     </>
   );
 };
@@ -54,6 +72,7 @@ const ImageListView: FC<Props> = ({ lightingCondition }) => {
 interface PropsMain {
   title: string;
   snap: QuerySnapshot<Image>;
+  setPage: Dispatch<SetStateAction<number>>;
 }
 
 type OperationMode = "pre" | "pro";
@@ -63,7 +82,7 @@ const operationModeLabelDict = {
   pro: "本番運用",
 };
 
-const ImageListViewMain: FC<PropsMain> = ({ title, snap }) => {
+const ImageListViewMain: FC<PropsMain> = ({ title, snap, setPage }) => {
   const images = snap.docs.map((doc) => ({
     ...doc.data(),
     ref: doc.ref,
@@ -82,7 +101,9 @@ const ImageListViewMain: FC<PropsMain> = ({ title, snap }) => {
     .filter((img) => filters.includes(img.operationMode as OperationMode));
 
   const updateFilter = (e: ChangeEvent<HTMLInputElement>) =>
-    e.target.checked ? setFilters([...filters, e.target.id as OperationMode]) : setFilters(filters.filter((key) => key !== e.target.id));
+    e.target.checked
+      ? setFilters([...filters, e.target.id as OperationMode])
+      : setFilters(filters.filter((key) => key !== e.target.id));
 
   const updateReview = (ref: DocumentReference<Image>, img: Image) => {
     updateDoc(ref, { humanLabel: img.humanLabel === "ok" ? "ng" : img.humanLabel === "ng" ? "ok" : img.predLabel });
@@ -91,7 +112,7 @@ const ImageListViewMain: FC<PropsMain> = ({ title, snap }) => {
   // const refreshDateRange = () => setDateRange([minDate, maxDate]);
 
   return (
-    <>
+    <div>
       <Stack mb={1} direction="row" alignItems="center" justifyContent="space-between">
         <Title>{title}</Title>
         {!sm && (
@@ -119,56 +140,61 @@ const ImageListViewMain: FC<PropsMain> = ({ title, snap }) => {
 
       {filterdImages.length === 0 && <Typography m="auto">⛔️ There is no images</Typography>}
       {filterdImages.length > 0 && (
-        <Box mr={4}>
-          <Masonry columns={{ xs: 2, sm: 3, md: 4, lg: 5, xl: 5 }} spacing={{ xs: 1 }}>
-            {/* <ImageList sx={{ width: !sm ? 1000 : 400, height: 580, marginX: "auto", my: 0 }} rowHeight={200} cols={!sm ? 5 : 2} gap={6}> */}
-            {filterdImages.map((img, i) => (
-              <div key={i}>
-                <ImageListItem sx={{ overflow: "hidden", maxHeight: "120px", minHeight: "80px" }}>
-                  <img key="2" src={`https://storage.googleapis.com/${img.bucket}/${img.dstPath}`} alt="img" loading="lazy" />
-                  {img.operationMode === "pre" && (
+        <>
+          <Box mr={4}>
+            <Masonry columns={{ xs: 2, sm: 3, md: 4, lg: 5, xl: 5 }} spacing={{ xs: 1 }}>
+              {/* <ImageList sx={{ width: !sm ? 1000 : 400, height: 580, marginX: "auto", my: 0 }} rowHeight={200} cols={!sm ? 5 : 2} gap={6}> */}
+              {filterdImages.map((img, i) => (
+                <div key={i}>
+                  <ImageListItem sx={{ overflow: "hidden", maxHeight: "120px", minHeight: "80px" }}>
+                    <img key="2" src={`https://storage.googleapis.com/${img.bucket}/${img.dstPath}`} alt="img" loading="lazy" />
+                    {img.operationMode === "pre" && (
+                      <ImageListItemBar
+                        sx={{
+                          background: "linear-gradient(to bottom, rgba(0,0,0,0.5) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+                        }}
+                        subtitle={"プレ運用"}
+                        position="top"
+                        actionIcon={
+                          <IconButton size="small" sx={{ color: "white" }} onClick={() => deleteDoc(img.ref)}>
+                            <Delete sx={{ fontSize: "18px" }} />
+                          </IconButton>
+                        }
+                      />
+                    )}
                     <ImageListItemBar
                       sx={{
-                        background: "linear-gradient(to bottom, rgba(0,0,0,0.5) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+                        background: "linear-gradient(to top, rgba(0,0,0,0.5) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
                       }}
-                      subtitle={"プレ運用"}
-                      position="top"
+                      title={
+                        img.predLabel ? img.predLabel.toUpperCase() + ": " + img.predConfidence : img.humanLabel.toUpperCase()
+                      }
+                      subtitle={img.createdAt.toDate().toLocaleString()}
                       actionIcon={
-                        <IconButton size="small" sx={{ color: "white" }} onClick={() => deleteDoc(img.ref)}>
-                          <Delete sx={{ fontSize: "18px" }} />
-                        </IconButton>
+                        img.operationMode === "pro" ? (
+                          <IconButton
+                            sx={{
+                              color: img.humanLabel ? colorDict(img.predLabel === img.humanLabel) : "rgba(255, 255, 255, 0.54)",
+                            }}
+                            onClick={() => updateReview(img.ref, img)}
+                          >
+                            {img.predLabel === img.humanLabel ? <CheckCircle /> : <Cancel />}
+                          </IconButton>
+                        ) : (
+                          <></>
+                        )
                       }
                     />
-                  )}
-                  <ImageListItemBar
-                    sx={{
-                      background: "linear-gradient(to top, rgba(0,0,0,0.5) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
-                    }}
-                    title={img.predLabel ? img.predLabel.toUpperCase() + ": " + img.predConfidence : img.humanLabel.toUpperCase()}
-                    subtitle={img.createdAt.toDate().toLocaleString()}
-                    actionIcon={
-                      img.operationMode === "pro" ? (
-                        <IconButton
-                          sx={{
-                            color: img.humanLabel ? colorDict(img.predLabel === img.humanLabel) : "rgba(255, 255, 255, 0.54)",
-                          }}
-                          onClick={() => updateReview(img.ref, img)}
-                        >
-                          {img.predLabel === img.humanLabel ? <CheckCircle /> : <Cancel />}
-                        </IconButton>
-                      ) : (
-                        <></>
-                      )
-                    }
-                  />
-                </ImageListItem>
-              </div>
-            ))}
-            {/* </ImageList> */}
-          </Masonry>
-        </Box>
+                  </ImageListItem>
+                </div>
+              ))}
+              {/* </ImageList> */}
+            </Masonry>
+          </Box>
+          {/* <InView onChange={(inView, entry) => inView && setPage((p) => p + 1)} /> */}
+        </>
       )}
-    </>
+    </div>
   );
 };
 
