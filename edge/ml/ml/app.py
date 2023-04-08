d986f9dc35029c26596c202f6ae9e89326e7a7a5
msrks <mshrrks@gmail.com>
2023-04-08 22:28:43 +0900

    add cors

--- edge/ml/ml/app.py
+++ edge/ml/ml/app.py
@@ -1,17 +1,26 @@
 import base64
-from typing import Any
 import cv2
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 from ml import model, pred_tf_saved_model, pred_tflite
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://visual-inspection-template.web.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 predModule = {"tflite": pred_tflite, "tf_saved_model": pred_tf_saved_model}
 
 
 @app.post("/predict", response_model=model.PredResult)
-async def predict(data: model.Data, modelType: model.ModelType) -> Any:
+async def predict(data: model.Data, modelType: model.ModelType) -> dict:
     im_array = np.frombuffer(base64.b64decode(data.image.encode()), dtype=np.uint8)
     im = cv2.imdecode(im_array, cv2.IMREAD_UNCHANGED)
     confidences, result = predModule[modelType].predict(im, "original")
